c8c2b4f031ce62a3cdd00c51398c498cd64be358
firstgenius <yaroslav.brovchenko@ucu.edu.ua>
2021-02-03 23:59:44 +0200

    adding left_to_right_check

--- skyscrapers.py
+++ skyscrapers.py
@@ -11,3 +11,25 @@ def read_input(path: str):
     for i in fil:
         res.append(i[:-1])
     return res
+
+def left_to_right_check(input_line: str, pivot: int):
+    """
+    Check row-wise visibility from left to right.
+    Return True if number of building from the left-most hint is visible looking to the right,
+    False otherwise.
+
+    input_line - representing board row.
+    pivot - number on the left-most hint of the input_line.
+
+    >>> left_to_right_check("412453*", 4)
+    True
+    >>> left_to_right_check("452453*", 5)
+    False
+    """
+    visibility = 1
+    highest = input_line[1]
+    for i in range(2,6,1):
+        if input_line[i] > highest:
+            visibility += 1
+            highest = input_line[i]
+    return visibility == pivot
